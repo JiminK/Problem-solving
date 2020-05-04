ff35bcd1f4b2c3e0b8449cb9832ffba9949f13c6
JiminK <39400030+JiminK@users.noreply.github.com>
2020-05-05 00:27:48 +0900

    upload BOJ 2839.java

---
+++ BOJ/no2839.java
@@ -0,0 +1,17 @@
+package 구현;
+import java.util.*;
+
+public class no2839 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int result = -1;
+		
+		
+		
+		
+		
+		System.out.println(result);
+	}
+
+}
