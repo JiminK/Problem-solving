70667f1dba8a0f890645185eb93f283f1c718a7f
JiminK <39400030+JiminK@users.noreply.github.com>
2020-05-14 18:18:16 +0900

    Upload BOJ 10171.java

---
+++ BOJ/no10171.java
@@ -0,0 +1,13 @@
+package 단계1;
+
+public class no10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
