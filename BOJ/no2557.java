1415e590ed46d083ca4643f8e41da52f4081455a
JiminK <39400030+JiminK@users.noreply.github.com>
2020-05-14 17:07:39 +0900

    Upload BOJ 2557.java

---
+++ BOJ/no2557.java
@@ -0,0 +1,11 @@
+package 단계1;
+import java.util.*;
+public class no2557 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hello World!");
+		sc.close();
+	}
+
+}
