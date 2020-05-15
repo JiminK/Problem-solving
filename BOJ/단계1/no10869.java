2869bd47a7795ad03e334523228b6d7ad8357386
JiminK <39400030+JiminK@users.noreply.github.com>
2020-05-16 01:16:14 +0900

    Upload BOJ 10869.java

---
+++ BOJ/단계1/no10869.java
@@ -0,0 +1,19 @@
+package 단계1;
+import java.util.*;
+public class no10869 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+		
+		sc.close();
+	}
+
+}
