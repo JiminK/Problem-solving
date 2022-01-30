89aa7c0fafa655fc7fd3e76948a7b306b8645de4
JiminK <jimin981005@gmail.com>
2022-01-31 02:44:44 +0900

    solve BOJ 14888

--- src/14888_2.cpp
+++ src/14888_2.cpp
@@ -9,26 +9,26 @@ long long maxResult = -1000000001, minResult = 1000000001;
 
 void dfs(int idx, long long result, int a, int s, int m, int d) {
     if (a > 0) {
-        cout << "+" << "\n";
-        result += numbers[idx + 1];
-        cout << "result : " << result << "\n";
-        dfs(idx + 1, result, a - 1, s, m, d);
+        // cout << "+" << "\n";
+        // result += numbers[idx + 1];
+        // cout << "result : " << result << "\n";
+        dfs(idx + 1, result + numbers[idx + 1], a - 1, s, m, d);
     }
     if (s > 0) {
-        cout << "-" << "\n";
-        result -= numbers[idx + 1];
-        dfs(idx + 1, result, a, s - 1, m, d);
+        // result -= numbers[idx + 1];
+        dfs(idx + 1, result - numbers[idx + 1], a, s - 1, m, d);
     }
     if (m > 0) {
-        cout << "*" << "\n";
-        result *= numbers[idx + 1];
-        cout << "result : " << result << "\n";
-        dfs(idx + 1, result, a, s, m - 1, d);
+        // cout << "*" << "\n";
+        // cout << "before result : " << result << "\n";
+        // cout << "numbers : " << numbers[idx] << ", " << numbers[idx + 1] << "\n";
+        // result *= numbers[idx + 1];
+        // cout << "after result : " << result << "\n";
+        dfs(idx + 1, result * numbers[idx + 1], a, s, m - 1, d);
     }
     if (d > 0) {
-        cout << "/" << "\n";
-        result /= numbers[idx + 1];
-        dfs(idx + 1, result, a, s, m, d - 1);
+        // result /= numbers[idx + 1];
+        dfs(idx + 1, result / numbers[idx + 1], a, s, m, d - 1);
     }
 
     if (a == 0 && s == 0 && m == 0 && d == 0) {
@@ -36,7 +36,6 @@ void dfs(int idx, long long result, int a, int s, int m, int d) {
         minResult = (result > minResult) ? minResult : result;
         // maxResult = max(result, maxResult);
         // minResult = min(result, minResult);
-        cout << "max: " << maxResult << "\n" << "min: " << minResult << "\n";
     }
 }
 
@@ -56,6 +55,7 @@ int main() {
         cin >> operators[i];
 
     dfs(0, numbers[0], operators[0], operators[1], operators[2], operators[3]);
+    cout << maxResult << "\n" << minResult << "\n";
 
     return 0;
 }
