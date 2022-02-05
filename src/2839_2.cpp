43f467d3b58353af311f729e6920d367e2a18b01
JiminK <jimin981005@gmail.com>
2022-02-05 22:45:06 +0900

    solve BOJ 2839

---
+++ src/2839_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    int cnt = 0;
+
+    while (n >= 0) {
+        if (n % 5 == 0) {
+            cnt += n / 5;
+            cout << cnt << "\n";
+            return 0;
+        } else {
+            n -= 3;
+            cnt++;
+        }
+    }
+    cout << -1 << "\n";
+    return 0;
+}
