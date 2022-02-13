00150087612939726638454268f0a6c60ea19ea6
JiminK <jimin981005@gmail.com>
2022-02-13 20:17:38 +0900

    solve BOJ 15650 Combination

--- src/14889_2.cpp
+++ src/14889_2.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #define MAX_N 20 + 2
+
+void combination(vector<int> v, vector<int> c, int r, int idx, int depth) {
+    if (r == 0) {
+        // cout << cnt + 1 << " : ";
+        for (int i = 0; i < c.size(); ++i) {
+            cout << c[i] << " ";
+        }
+        cout << "\n"; 
+        // cnt++;
+    } else if (depth == v.size()) {
+        return;
+    } else {
+        c[idx] = v[depth];
+        combination(v, c, r - 1, idx + 1, depth + 1);
+        combination(v, c, r, idx, depth + 1);
+    }
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -9,20 +28,28 @@ int main() {
     int n;
     cin >> n;
     int halfN = n / 2;
-    int sij[MAX_N][MAX_N] = {0};
+    vector< vector<int> > sij(n, vector<int>(n, 0));
 
-    for (int i = 1; i < n; ++i) {
-        for (int j = 1; j < n; ++j) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << sij[i][j] << " ";
+
+        }
+        cout << "\n";
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
             cin >> sij[i][j];
         }
     }
 
     int sTeam = 0, lTeam = 0;
-    for (int i = 1; i < n + 1; ++i) {
-        for (int j = 1; j < n + 1; ++j) {
-            
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << sij[i][j] << " ";
 
         }
+        cout << "\n";
     }
 
     return 0;
--- src/15650.cpp
+++ src/15650.cpp
@@ -3,12 +3,15 @@
 
 using namespace std;
 
+// int cnt = 0;
 void combination(vector<int> v, vector<int> c, int r, int idx, int depth) {
     if (r == 0) {
+        // cout << cnt + 1 << " : ";
         for (int i = 0; i < c.size(); ++i) {
             cout << c[i] << " ";
         }
-        cout << "\n";
+        cout << "\n"; 
+        // cnt++;
     } else if (depth == v.size()) {
         return;
     } else {
@@ -16,13 +19,12 @@ void combination(vector<int> v, vector<int> c, int r, int idx, int depth) {
         combination(v, c, r - 1, idx + 1, depth + 1);
         combination(v, c, r, idx, depth + 1);
     }
-
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    
+
     int n, m; cin >> n >> m;
     vector<int> v(n), c(m);
     for (int i = 1; i <= n; ++i) {
@@ -33,6 +35,7 @@ int main() {
     // }
 
     combination(v, c, m, 0, 0);
+    // cout << "cnt : " << cnt << "\n";
 
     return 0;
 }
