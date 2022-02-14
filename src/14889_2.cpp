0de79302cc1887dfc7f6275a9635acfd5f1d596f
JiminK <jimin981005@gmail.com>
2022-02-14 21:02:48 +0900

    found team combination

--- src/14889_2.cpp
+++ src/14889_2.cpp
@@ -2,22 +2,37 @@
 #include <vector>
 using namespace std;
 
-#define MAX_N 20 + 2
+#define MAX_N 22
 
-void combination(vector<int> v, vector<int> c, int r, int idx, int depth) {
+// vector< vector<int> > sij(MAX_N, vector<int>(MAX_N, 0));
+vector< vector<int> > sij;
+// int sTeam[MAX_N / 2], lTeam[MAX_N / 2]; // team combination
+vector< vector<int> > sTeam;
+vector< vector<int> > lTeam;
+vector<int> member;
+vector<int> c;
+
+void combination(vector<int> v, vector<int> c, int r, int idx, int depth, int halfN) {
     if (r == 0) {
-        // cout << cnt + 1 << " : ";
+        vector<int> sTmp;
+        vector<int> lTmp;
         for (int i = 0; i < c.size(); ++i) {
-            cout << c[i] << " ";
-        }
-        cout << "\n"; 
-        // cnt++;
+            cout << c[i] << ",";
+            if (c[i] != 0) { // sTeam
+                sTmp.push_back(c[i]);
+                
+            } else { // lTeam
+                lTmp.push_back(v[i]);
+            }
+        }cout << "\n";
+        sTeam.push_back(sTmp);
+        lTeam.push_back(lTmp);
     } else if (depth == v.size()) {
         return;
     } else {
         c[idx] = v[depth];
-        combination(v, c, r - 1, idx + 1, depth + 1);
-        combination(v, c, r, idx, depth + 1);
+        combination(v, c, r - 1, idx + 1, depth + 1, halfN);
+        combination(v, c, r, idx, depth + 1, halfN);
     }
 }
 
@@ -25,32 +40,36 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n;
-    cin >> n;
+    int n; cin >> n;
+    // cout << "n: " << n << "\n";
     int halfN = n / 2;
-    vector< vector<int> > sij(n, vector<int>(n, 0));
+    // cout << "halfN: " << halfN;
 
     for (int i = 0; i < n; ++i) {
+        member.push_back(i + 1);
+    }
+    for (int i = 0; i < n; ++i) {
+        vector<int> tmp;
         for (int j = 0; j < n; ++j) {
-            cout << sij[i][j] << " ";
-
+            int tempNum;
+            cin >> tempNum;
+            tmp.push_back(tempNum);
+        }
+        sij.push_back(tmp);
+    }
+    for (int i = 0; i < n; ++i) {
+        c.push_back(0);
+    }
+    combination(member, c, halfN, 0, 0, halfN);
+    for (int i = 0; i < sTeam.size(); ++i) {
+        for (int j = 0; j < halfN; ++j) {
+            cout << sTeam[i][j] << " ";
         }
         cout << "\n";
     }
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin >> sij[i][j];
-        }
-    }
 
-    int sTeam = 0, lTeam = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cout << sij[i][j] << " ";
-
-        }
-        cout << "\n";
-    }
+    int sTeamScore = 0, lTeamScore = 0;
+    
 
     return 0;
 }
--- src/temp.cpp
+++ src/temp.cpp
@@ -3,13 +3,33 @@
 
 using namespace std;
 
-void combination(vector<int> v, vector<int> c, int idx, int depth) {
-    
-
-}
 
 int main() {
+    int n;
+    cin >> n;
+    vector< vector<int> > sij(n, vector<int>(n, 0));
 
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << sij[i][j] << " ";
+
+        }
+        cout << "\n";
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> sij[i][j];
+        }
+    }
+
+    int sTeam = 0, lTeam = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << sij[i][j] << " ";
+
+        }
+        cout << "\n";
+    }
 
     return 0;
 }
