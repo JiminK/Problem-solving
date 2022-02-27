ed9fa114fe0489b23b098f72f2241ae8285d6868
JiminK <jimin981005@gmail.com>
2022-02-28 01:37:21 +0900

    solve BOJ 13164 with help

--- src/13164.cpp
+++ src/13164.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    // ios::sync_with_stdio(false);
-    // cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
     int n, k; cin >> n >> k;
-    
-    
+    vector<int> heights(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> heights[i];
+    }
+
+    vector<int> diffHeight(n - 1);
+    for (int i = 0; i < n - 1; ++i) {
+        diffHeight[i] = heights[i + 1] - heights[i];
+    }
+    sort(diffHeight.begin(), diffHeight.end());
+
+    long long minCost = 0;
+    for (int i = 0; i < n - k; ++i) {
+        minCost += diffHeight[i];
+    }
+    cout << minCost << "\n";
 
     return 0;
 }
