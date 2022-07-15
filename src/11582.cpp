da466ab03e13ae6b6038aaad5bb3463ce5341040
JiminK <jimin981005@gmail.com>
2022-07-15 20:57:20 +0900

    solve BOJ 11582

---
+++ src/11582.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N, K;
+vector<int> vec;
+
+void getInput() {
+    cin >> N;
+    for (int i=0; i<N; ++i) {
+        int temp; cin >> temp;
+        vec.push_back(temp);
+    }
+    cin >> K;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    getInput();
+
+    for (int i=0; i<N; i+=N/K) { 
+        sort(vec.begin()+i, vec.begin()+i+N/K);
+    }
+    for (int i=0; i<N; i++) {
+        cout << vec[i] << " ";
+    }
+    cout << "\n";
+    // for (auto iter = vec.begin(); iter != vec.end(); iter += N/K){
+    //     sort(iter, iter + N/K);
+    // }
+
+    return 0;
+}
