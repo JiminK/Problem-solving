af160fcd9e9ecb39fa093992a2799c02861b989e
JiminK <jimin981005@gmail.com>
2022-07-24 19:23:07 +0900

    solve BOJ 15565 (no two-pointer)

--- src/15565.cpp
+++ src/15565.cpp
@@ -5,37 +5,7 @@
 
 using namespace std;
 
-// void solve() {
-//     // cout << idx << "==========\n";
-//     int p = idx, q = idx + 1, cnt = 1;
-//     while (true) {
-//         // cout << "cnt: " << cnt << "==========\n";
-//         if (cnt == K) {
-//             answer = q - p - 1;
-//             // cout << "answer: " << answer << "==========\n";
-//             if (answer < minAnswer) minAnswer = answer;
-//         }
-//         // if (vec[p] == 1) {
-//         //     ++q;
-//         // } 
-//         if (vec[q] == 1) {
-//             ++cnt;
-//         }
-//         if (p >= N) return;
-//         if (q >= N) {
-//             if (vec[p] == 1) {
-//                 ++cnt;
-//             } 
-//             ++p;
-//             --cnt;
-//             continue;
-//         }
-//         ++q;
-//     }
-// }
-
 int N, K, idx;
-vector<int> vec;
 int answer = -1;
 int minAnswer = MAX_N + 5;
 vector<int> isRyan;
@@ -57,17 +27,12 @@ void getInput() {
     cin >> N >> K;
     for (int i=0; i<N; ++i) {
         int temp; cin >> temp;
-        vec.push_back(temp);
         // 라이언(1) index 저장 
         if (temp == 1) {
             isRyan.push_back(i);
         }
     }
-    // for (int i=0; i<isRyan.size(); ++i) {
-    //     cout << isRyan[i] << " ";
-    // }
     getDiff();
-    // solve();
 }
 
 int main() {
