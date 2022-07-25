2b23a4d3988f31f348a9fdf63db945f9ca83aa94
JiminK <jimin981005@gmail.com>
2022-07-25 22:55:30 +0900

    solve BOJ 18870 : refer

--- src/18870.cpp
+++ src/18870.cpp
@@ -5,51 +5,29 @@
 using namespace std;
 
 int N;
-struct strt {
-    int idx;
-    int xi;
-    int newXi;
-
-    bool operator > (const strt &s) const {
-        return xi > s.xi;
-    }
-    bool operator < (const strt &s) const {
-        return xi < s.xi;
-    }
-};
-vector<strt> vec;
-
-bool comp(strt &a, strt &b) {
-    return a.idx < b.idx;
-}
+vector<int> vec;
+vector<int> vec2;
 
 void solve() {
-    vec[0].newXi = 0;
-    int temp = vec[0].xi;
-    for (int i=1; i<N; ++i) {
-        vec[i].newXi = i;
-        if (vec[i].xi == temp) {
-            vec[i].newXi = vec[i-1].newXi;
-        }
-        temp = vec[i].xi;
+    // 중복 제거 
+    vec2.erase(unique(vec2.begin(), vec2.end()), vec2.end());
+    
+    for (int i=0; i<N; ++i) {
+        cout << lower_bound(vec2.begin(), vec2.end(), vec[i]) - vec2.begin() << " ";
     }
-}
+    cout << "\n";
+} 
 
 void getInput() {
     cin >> N;
     for (int i=0; i<N; ++i) {
-        strt temp; temp.idx = i; cin >> temp.xi;
+        int temp; cin >> temp;
         vec.push_back(temp);
+        vec2.push_back(temp);
     }
-    sort(vec.begin(), vec.end());
+    // sort(vec.begin(), vec.end());
+    sort(vec2.begin(), vec2.end());
     solve();
-    // for (int i=0; i<N; ++i) {
-    //     cout << vec[i].idx << " " << vec[i].xi << " " << vec[i].newXi << "\n";
-    // }
-    sort(vec.begin(), vec.end(), comp);
-    for (int i=0; i<N; ++i) {
-        cout << vec[i].idx << " " << vec[i].xi << " " << vec[i].newXi << "\n";
-    }
 }
 
 int main() {
@@ -63,3 +41,4 @@ int main() {
 
 // https://chanhuiseok.github.io/posts/algo-55/ : 참고 링크 
 // https://programmers.co.kr/learn/courses/30/lessons/60060 : 카카오 기출 문제 
+// https://chanhuiseok.github.io/posts/algo-55/ : lower_bound 
