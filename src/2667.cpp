9c0578979a6ca39cc40403d259273f4c2376c320
JiminK <jimin981005@gmail.com>
2022-09-24 23:01:57 +0900

    solve BOJ 2667 : refer

--- src/2667.cpp
+++ src/2667.cpp
@@ -1,38 +1,72 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // r, d, l, u
-bool visited[25][25];
+int N;
+int map[26][26];
+vector<int> danji; // size 출력, 원소 오름차순 출력
+int dir[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+bool visited[26][26];
+int danjiNum;
 
-int dfs(int row, int col) {
-    if (row < 0 || row > 25 || col < 0 || col > 25){
-        return 0;
+int dfs(int r, int c, int cnt) {
+    if (visited[r][c]) return 0;
+    visited[r][c] = 1;
+    for (int i=0; i<4; ++i) {
+        int nextR = r + dir[i][0], nextC = c + dir[i][1];
+        if (nextR < 0 || nextC < 0 || nextR >= N || nextC >= N) continue;
+        if (map[nextR][nextC] == 0) continue;
+        if (visited[nextR][nextC]) continue;
+        // cout << "cnt : " << cnt << "\n";
+        // danjiNum += dfs(nextR, nextC, cnt+1);
+        cnt = dfs(nextR, nextC, cnt+1);
     }
-    if (visited[row][col]){
-        return 0;
-    }
+    return cnt;
+}
 
-    for(int i=4;;){
-        int nextRow = row + dir[i][0];
-        int nextCol = col + dir[i][1];
-        if(visited[nextRow][nextCol]){
-            continue;
+void init() {
+    for (int i=0; i<N; ++i) {
+        for (int j=0; j<N; ++j) {
+            visited[i][j] = 0;
         }
     }
 }
 
-int main() {
-    int n;
-    int arr[25][25];
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-           cin >> arr[i][j];
+void solve() {
+    for (int r=0; r<N; ++r) {
+        for (int c=0; c<N; ++c) {
+            if (map[r][c] == 0 || visited[r][c] == 1) continue;
+            danjiNum = dfs(r, c, 0);
+            // cout << "final cnt >>>>>>>> " << danjiNum+1 << "\n";
+            danji.push_back(danjiNum+1);
+        }   
+    }
+    cout << danji.size() << "\n";
+    sort(danji.begin(), danji.end());
+    for (int i=0; i<danji.size(); ++i) {
+        cout << danji[i] << "\n";
+    }
+}
+
+void getInput() {
+    cin >> N;
+    for (int i=0; i<N; ++i) {
+        string tempStr; cin >> tempStr;
+        for (int j=0; j<N; ++j) {
+            map[i][j] = tempStr[j] - '0';
         }
     }
+    solve();
+}
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-
+    getInput();
 
     return 0;
 }
