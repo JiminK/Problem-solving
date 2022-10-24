da4a42874554a9e1f6564270b67097ad5b6fdda0
JiminK <jimin981005@gmail.com>
2022-10-24 12:32:05 +0900

    disjoint-set

--- src/algorithms/disjoint_set.cpp
+++ src/algorithms/disjoint_set.cpp
@@ -4,32 +4,34 @@
 
 using namespace std;
 
-int par[MAXN], cnt[MAXN];
+int root[MAXN], cnt[MAXN];
 
 int find(int v) {
-  if (par[v] == v) return v;
-  int r = find(par[v]);
+  if (root[v] == v) return v;
+  int r = find(root[v]);
   // 좌표압축
-  par[v] = r;
+  root[v] = r;
   return r;
 }
 
 void union_(int v, int u) {
-  int vr = find(v);
-  int ur = find(u);
-  if (vr == ur) return;
-  if (cnt[vr] < cnt[ur]) {
-    par[vr] = ur;
-    cnt[ur] += cnt[vr];
-  } else { // cnt[vr] >= cnt[ur]
-    par[ur] = vr;
-    cnt[vr] += cnt[ur];
+  int vRoot = find(v);
+  int uRoot = find(u);
+  // v와 u의 루트가 같다 = 이미 같은 집합이다
+  if (vRoot == uRoot) return;
+  // 루트가 다르면, 원소의 개수가 더 많은 집합의 루트의 자식으로 합친다. 
+  if (cnt[vRoot] < cnt[uRoot]) {
+    root[vRoot] = uRoot;
+    cnt[uRoot] += cnt[vRoot];
+  } else { // cnt[vRoot] >= cnt[uRoot]
+    root[uRoot] = vRoot;
+    cnt[vRoot] += cnt[uRoot];
   }
 }
 
 int main() {
   for (int i=0; i<MAXN; ++i) {
-    par[i] = i;
+    root[i] = i;
     cnt[i] = 1;
   }
   return 0;
