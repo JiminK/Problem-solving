9b5e39720d10b0ee91ef166710e926f9aad56b53
JiminK <jimin981005@gmail.com>
2022-10-25 19:26:06 +0900

    solve BOJ 9625

--- src/9625_2.cpp
+++ src/9625_2.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-#include <string>
 
 using namespace std; 
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
+int N;
+int DP[50][2];
 
-	int n; cin >> n;
-	int DP[50][2];
- 
+void solve() {
   // 1번 눌렀을 때, A -> B
 	DP[1][0] = 0; // A의 개수
 	DP[1][1] = 1; // B의 개수
@@ -17,12 +13,25 @@ int main() {
 	DP[2][0] = 1; 
 	DP[2][1] = 1; 
  
-	for (int i=3; i<=n; ++i) {
+	for (int i=3; i<=N; ++i) {
 		DP[i][0] = DP[i - 1][0] + DP[i - 2][0];
 		DP[i][1] = DP[i - 1][1] + DP[i - 2][1];
 	}
+
+}
+
+void getInput() {
+  cin >> N;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  getInput();
+  solve();
  
-	cout << DP[n][0] << " " << DP[n][1] << "\n";
+	cout << DP[N][0] << " " << DP[N][1] << "\n";
  
 	return 0;
 }
